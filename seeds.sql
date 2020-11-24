a1379ca317c3f391d27a34c0a09c922791877951
reedtlr <69362002+reedtlr@users.noreply.github.com>
2020-11-24 11:52:30 -0500

    add seed file

---
+++ seeds.sql
@@ -0,0 +1,10 @@
+USE employee_track_DB;
+
+INSERT INTO employee (position, first_name, last_name, rolde_id, manager_id, title, salary, department_id, department)
+VALUES (1, "Martha", "Smith", 1, 1, "lead maanger", 100000, 1, "Executive");
+
+INSERT INTO employee (position, first_name, last_name, rolde_id, manager_id, title, salary, department_id, department)
+VALUES (2, "John", "Doe", 4, 2, "sales associate", 35000, 2, "Sales");
+
+INSERT INTO employee (position, first_name, last_name, rolde_id, manager_id, title, salary, department_id, department)
+VALUES (3, "Jill", "Jones", 3, 2, "sales manager", 50000, 2, "Sales");
